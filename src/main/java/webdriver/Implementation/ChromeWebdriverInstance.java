0414872ee0f09c637fef7777b55518b0de9539f9
Dzmitry <dzmitry@segpay.com>
2020-11-19 03:48:02 -0500

    upodated chromedriver version to the lates stable 87.0.4280.20

--- src/main/java/webdriver/Implementation/ChromeWebdriverInstance.java
+++ src/main/java/webdriver/Implementation/ChromeWebdriverInstance.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import webdriver.Abstraction.IWebDriverInstanceInit;
 
 public class ChromeWebdriverInstance implements IWebDriverInstanceInit {
-    public static final String VERSION_CHROME_DRIVER = "86.0.4240.22";
+    public static final String VERSION_CHROME_DRIVER = "87.0.4280.20";
     @Override
     public WebDriver initWebDriverInstance() {
         io.github.bonigarcia.wdm.WebDriverManager.chromedriver().browserVersion(VERSION_CHROME_DRIVER).setup();
